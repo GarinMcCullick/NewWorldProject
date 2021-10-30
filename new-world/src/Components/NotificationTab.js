6bb896fd1f64664c4c8571a1bdeac6062d78d00e
Garin Mccullick <glmccullick@gmail.com>
2021-10-30 17:06:36 -0500

    added draggable notifications

--- new-world/src/Components/NotificationTab.js
+++ new-world/src/Components/NotificationTab.js
@@ -1,27 +1,38 @@
 import React from "react"
 import styled from "styled-components"
+import Draggable from 'react-draggable'
+
+const DragWrapper = styled.div`
+position:absolute;
+width:100%;
+height:100%;
+display:flex;
+justify-content:end;
+align-items:end;
+`
 
 const Notifications = styled.div`
     display:flex;
     flex-flow:column nowrap;
     background-color:white;
-    margin-top:50px;
-    margin-right:30px;
     width:15%;
     height:400px;
     border:1px solid black;
     justify-content:start;
     align-items:center;
     background-color:darkgrey;
+    position:relative;
+    z-index: 10;
 `
 
 const Title = styled.div`
-    width:60%;
+    width:100%;
     height:auto;
-    margin-top:1rem;
     text-align:center;
     background-color:grey;
     color:white;
+    cursor:move;
+    user-select:none;
 `
 
 const Feed = styled.div`
@@ -30,6 +41,7 @@ const Feed = styled.div`
     height:50px;
     margin-top:5%;
     border-radius: 25px;
+    cursor:pointer;
 `
 
 const P = styled.p`
@@ -37,12 +49,20 @@ const P = styled.p`
     margin-left:0.5rem;
 `
 
+const P1 = styled.p`
+    font-size:14px;
+    color:lightgrey;
+`
+
 const NotificationTab = () =>{
 
 return(
+<DragWrapper>
+<Draggable handle='#handle' bounds='parent'>
 <Notifications>
-                <Title>
+                <Title id='handle'>
                     notifications
+                    <P1>(Drag Me)</P1>
                 </Title>
                 <Feed>
                     <P>notifications</P>
@@ -60,6 +80,8 @@ return(
                     <P>notifications</P>
                 </Feed>
 </Notifications>
+</Draggable>
+</DragWrapper>
 )}
 
 export default NotificationTab
